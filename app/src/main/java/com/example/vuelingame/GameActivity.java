9623077632200a0580b0f59e21a4728456e3480d
eduardpopa99 <eduardpopa4@gmail.com>
2019-10-13 07:06:43 +0200

    Version 1.0

--- app/src/main/java/com/example/vuelingame/GameActivity.java
+++ app/src/main/java/com/example/vuelingame/GameActivity.java
@@ -22,6 +22,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
